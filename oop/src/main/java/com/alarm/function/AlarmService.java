09387d4132fb08573e14813f280b578eea25f9ae
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-28 14:21:20 +0900

    분리...

--- oop/src/main/java/com/alarm/function/AlarmService.java
+++ oop/src/main/java/com/alarm/function/AlarmService.java
@@ -2,18 +2,30 @@ package com.alarm.function;
 
 public class AlarmService {
 
+    public void setup(){
+        System.out.println("알람을 시작합니다.");//초기화
+    }
+    
+    public void reset(){
+        System.out.println("재시작합니다..."); //리셋
+    }
+
+    public void loop(int time) {
+        if (time == 10) {
+            System.out.println("10시가 되엇습니다.");
+        }
+        if (time == 17) {
+            System.out.println("17시가 되엇습니다.");
+        }
+    }
+    
     public void run() throws InterruptedException {
-        System.out.println("알람을 시작합니다.");
+        setup();
         while (true) {
-            System.out.println("재시작합니다...");
-            for (int i = 0; i < 24; i++) {
+            reset();
+            for (int time = 0; time < 24; time++) {
                 Thread.sleep(100); //0.1초 쉬어감
-                if (i == 10) {
-                    System.out.println("10시가 되엇습니다.");
-                }
-                if (i == 17) {
-                    System.out.println("17시가 되엇습니다.");
-                }
+                loop(time);
             }
         }
     }
