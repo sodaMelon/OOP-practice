a2e105030bfa0e8c88e74075c588ffa0e19e4a13
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-28 17:48:17 +0900

    anotation 정보 읽어오기

--- oop/src/main/java/com/alarm/annotaion/AlarmTest.java
+++ oop/src/main/java/com/alarm/annotaion/AlarmTest.java
@@ -3,19 +3,20 @@ package com.alarm.annotaion;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-class Alarm{
-    @Task
-    public void  task0(){
+class Alarm {
+    @Task(time = 10)
+    public void task0() {
         System.out.println("^^^^^^^");
     }
 
-    @Task @Log
-    public void  task1(){
+    @Task(time = 40)
+    @Log
+    public void task1() {
         System.out.println("###########");
     }
 
     @Log
-    public void func1(){
+    public void func1() {
         System.out.println("no~~~~");
     }
 }
@@ -28,10 +29,10 @@ public class AlarmTest {
         //System.out.println("------------------");
         Method methods[] = alarm.getClass().getDeclaredMethods();
 
-        for (int i=0; i<methods.length; i++){
+        for (int i = 0; i < methods.length; i++) {
             System.out.println(methods[i].getName());
         }
-        for (Method method: methods){
+        for (Method method : methods) {
             System.out.println(method.getName());
 
             Task annTask = method.getAnnotation(Task.class);
@@ -44,7 +45,10 @@ public class AlarmTest {
             else System.out.println("@Log");
 
             if (annTask != null) {//리플렉션해서 함수 실행해보긔
-                method.invoke(alarm); //첫번째 파라미터에 이 함수를 소유하고있는 실제 객체,
+                System.out.println("Time value" + annTask.time());
+                if (annTask.time() == 10) {
+                    method.invoke(alarm); //첫번째 파라미터에 이 함수를 소유하고있는 실제 객체,
+                 }
             }
         }
     }
--- oop/src/main/java/com/alarm/annotaion/Task.java
+++ oop/src/main/java/com/alarm/annotaion/Task.java
@@ -5,4 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME) // 타입별로 기능이 다름
 public @interface Task { //define annotaion
+    public int time();
 }
