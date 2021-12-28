b8d613ce18a6e7d1b5ab7ce8d25b42fd4c28e1bc
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-28 17:38:46 +0900

    어노테이션 & 리플렉션 활용

---
+++ oop/src/main/java/com/alarm/annotaion/AlarmTest.java
@@ -0,0 +1,51 @@
+package com.alarm.annotaion;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+class Alarm{
+    @Task
+    public void  task0(){
+        System.out.println("^^^^^^^");
+    }
+
+    @Task @Log
+    public void  task1(){
+        System.out.println("###########");
+    }
+
+    @Log
+    public void func1(){
+        System.out.println("no~~~~");
+    }
+}
+
+public class AlarmTest {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Alarm alarm = new Alarm();
+        //alarm.func1();
+        //alarm.task0();
+        //System.out.println("------------------");
+        Method methods[] = alarm.getClass().getDeclaredMethods();
+
+        for (int i=0; i<methods.length; i++){
+            System.out.println(methods[i].getName());
+        }
+        for (Method method: methods){
+            System.out.println(method.getName());
+
+            Task annTask = method.getAnnotation(Task.class);
+            //값이 있으면 반환 없으면 null.. 이거 해킹에 쓰지않나...??
+            if (annTask == null) System.out.println("x");
+            else System.out.println("@Task");
+
+            Log annLog = method.getAnnotation(Log.class);
+            if (annLog == null) System.out.println("x");
+            else System.out.println("@Log");
+
+            if (annTask != null) {//리플렉션해서 함수 실행해보긔
+                method.invoke(alarm); //첫번째 파라미터에 이 함수를 소유하고있는 실제 객체,
+            }
+        }
+    }
+}
---
+++ oop/src/main/java/com/alarm/annotaion/Task.java
@@ -0,0 +1,8 @@
+package com.alarm.annotaion;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // 타입별로 기능이 다름
+public @interface Task { //define annotaion
+}
