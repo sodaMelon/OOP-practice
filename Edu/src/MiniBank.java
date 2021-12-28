c20a6fe5250ef513bcdfaffa064aa426277ae977
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-28 12:21:49 +0900

    주석 정리

--- Edu/src/MiniBank.java
+++ Edu/src/MiniBank.java
@@ -18,7 +18,7 @@ public class MiniBank {
     }
 }
 
-class Account {
+class Account implements IAccount {
     private int balance;
 
     public int getBalance() {
@@ -29,32 +29,23 @@ class Account {
         balance += value;
     }
 
-    public int withrawl(int value) throws Exception { //thorw를 여러개 바깥으로 던져줄수있어서 s붙음.? 아니 이건 객체라그렇고
+    public int withrawl(int value) throws Exception {
         if (balance > value) {
             balance -= value;
             return balance;
         }
-        throw new Exception("예외 발생 ㅇㅅㅇ;"); //여긴 명령문이라 그렇고가아니라?????()
+        throw new Exception("예외 발생 ㅇㅅㅇ;");
     }
-
 }
 
+interface IAccount {
+    public int withrawl(int value) throws Exception;
+    public void deposit(int value);
+    public int getBalance();
+}
 
-class WRAccount { //상속연습 준비
+class WRAccount extends Account{ //상속연습 준비
 
-    private int balance;
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void deposit(int value) {
-        balance += value * 1.1f;
-    }
-
-    public void withdraw(int value) {
-        balance -= value;
-    }
 }
 
 class KBAccount{ //상속연습 준비2
