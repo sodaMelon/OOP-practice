b94337932d289f2e8045170fd1e6774d4b0d8854
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-29 09:21:21 +0900

    service 완성

--- oop/src/main/java/com/alarm/annotaion/AlarmTest.java
+++ oop/src/main/java/com/alarm/annotaion/AlarmTest.java
@@ -1,54 +1,87 @@
-package com.alarm.annotaion;
+
+
+import com.alarm.annotaion.Log;
+import com.alarm.annotaion.Task;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 class Alarm {
-    @Task(time = 10)
-    public void task0() {
-        System.out.println("^^^^^^^");
+
+    @Task(time=10)
+    public  void task0() {
+        System.out.println("10시 입니다.");
     }
 
-    @Task(time = 40)
     @Log
+    @Task(time=14)
     public void task1() {
-        System.out.println("###########");
+        System.out.println("14시 입니다.");
     }
 
-    @Log
-    public void func1() {
-        System.out.println("no~~~~");
+    public  void fun1() {
+        System.out.println("no~~~~~~~~~~~~~");
     }
 }
-
 public class AlarmTest {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InterruptedException {
+        new AlarmService(new Alarm()).run();
+    }
+
+
+    public static void main2(String[] args) throws InvocationTargetException, IllegalAccessException {
+
         Alarm alarm = new Alarm();
-        //alarm.func1();
-        //alarm.task0();
-        //System.out.println("------------------");
         Method methods[] = alarm.getClass().getDeclaredMethods();
 
-        for (int i = 0; i < methods.length; i++) {
-            System.out.println(methods[i].getName());
-        }
-        for (Method method : methods) {
+        for(Method method : methods) {
             System.out.println(method.getName());
-
             Task annTask = method.getAnnotation(Task.class);
-            //값이 있으면 반환 없으면 null.. 이거 해킹에 쓰지않나...??
-            if (annTask == null) System.out.println("x");
+            if ( annTask == null) System.out.println("X");
             else System.out.println("@Task");
 
             Log annLog = method.getAnnotation(Log.class);
-            if (annLog == null) System.out.println("x");
+            if ( annLog == null) System.out.println("X");
             else System.out.println("@Log");
 
-            if (annTask != null) {//리플렉션해서 함수 실행해보긔
-                System.out.println("Time value" + annTask.time());
-                if (annTask.time() == 10) {
-                    method.invoke(alarm); //첫번째 파라미터에 이 함수를 소유하고있는 실제 객체,
-                 }
+            if ( annTask != null) {
+                System.out.println("time value =" + annTask.time());
+                if ( annTask.time() == 10 )
+                    method.invoke(alarm);
+            }
+        }
+    }
+}
+
+class AlarmService {
+
+    private  Object alarm;
+
+    public  AlarmService(Object alarm) {
+        this.alarm = alarm;
+
+    }
+    public  void run() throws InvocationTargetException, IllegalAccessException, InterruptedException {
+        Method methods[] = alarm.getClass().getDeclaredMethods();
+        HashMap<Integer, Method> mapTask = new HashMap<Integer,Method>();
+
+        for (Method method : methods) {
+            Task antTask = method.getAnnotation(Task.class);
+            if (antTask != null) {
+                mapTask.put(antTask.time(), method);
+            }
+        }
+        while (true) {
+            for (int i = 0; i < 24; i++) {
+                Thread.sleep(100);
+                for (Integer time: mapTask.keySet() ) {
+                    if ( i == time )  {
+                        Method method = mapTask.get(time);
+                        method.invoke(alarm);
+                    }
+                }
             }
         }
     }
