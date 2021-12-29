2a89aa78c8ded7f5f6ad74ced35530e3394b7517
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-29 12:16:33 +0900

    check generate bean(with scope)

--- spring/demo/src/main/java/com/spring/demo/DemoApplication.java
+++ spring/demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -27,7 +27,13 @@ public class DemoApplication {
         if (p1 ==p1_2) System.out.println("동일주소");
         else System.out.println("다른 주소");
 
-        Person p2 =  (Person) context.getBean("p2");
-        System.out.println(p2);
+        Person p2 =  (Person) context.getBean("p2"); //<-여기 주석처리해도 싱글톤으로 동작하기때문에
+        System.out.println(p2); // 객체 생성시점 : container에 의해서 xml이 읽어질때 생성됨..
+
+        //prototype은 런타임 시에 생성
+
+        context.close();//안정적으로 context 닫음 (없으면 강제종료...~)
+        //prototype에 의해 생성하게되면 container가 생성한게 아님...-> 자바자체가 생성한거기때문에 GC가 동작할때까지 그대로 있게됨
+        //따라서 Person에 정의한 init, destroy는 container가 싱글톤으로 관리할때만 생김
     }
 }
