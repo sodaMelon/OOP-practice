83e667f260c3b350101a3e3c66e010c5cb35176b
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-29 13:47:50 +0900

    prepare to manage  new Bean (Car)

--- spring/demo/src/main/java/com/spring/demo/DemoApplication.java
+++ spring/demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -26,14 +28,19 @@ public class DemoApplication {
         System.out.println(p1_2); // p1 = p1_2가 같음.. (이미 생성된 객체 다시 사용)
         if (p1 ==p1_2) System.out.println("동일주소");
         else System.out.println("다른 주소");
+        Person p2 =  (Person) context.getBean("p2");
+        System.out.println(p2);
+/////////////////////////////////////////////////////////
+        Car car = new Car("소나타", "빨간색");
+        ArrayList<Option> opts = new ArrayList<Option>();
+        opts.add(new Option("네비게이션"));
+        opts.add(new Option("후방 카메라"));
+        opts.add(new Option("AV 시스템"));
+        car.setOptions(opts);
 
-        Person p2 =  (Person) context.getBean("p2"); //<-여기 주석처리해도 싱글톤으로 동작하기때문에
-        System.out.println(p2); // 객체 생성시점 : container에 의해서 xml이 읽어질때 생성됨..
+        System.out.println(car);
 
-        //prototype은 런타임 시에 생성
 
-        context.close();//안정적으로 context 닫음 (없으면 강제종료...~)
-        //prototype에 의해 생성하게되면 container가 생성한게 아님...-> 자바자체가 생성한거기때문에 GC가 동작할때까지 그대로 있게됨
-        //따라서 Person에 정의한 init, destroy는 container가 싱글톤으로 관리할때만 생김
+        context.close();
     }
 }
---
+++ spring/demo/src/main/java/com/spring/demo/Option.java
@@ -0,0 +1,10 @@
+package com.spring.demo;
+
+
+import lombok.*;
+
+@Getter @Setter @ToString @NoArgsConstructor
+@AllArgsConstructor
+public class Option {
+    private String name;
+}
