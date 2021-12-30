d7c79906537940e5bd7490541bf8c73796955074
DESKTOP-NOVH08U\user <sodamlee315@gmail.com>
2021-12-30 11:29:13 +0900

    set home page

---
+++ Newsletter/news/src/main/java/com/spring/news/Banner.java
@@ -0,0 +1,14 @@
+package com.spring.news;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Banner {
+    private  int id;
+    private  String img;
+    private  String url;
+}
